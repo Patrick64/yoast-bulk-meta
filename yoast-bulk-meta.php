a8bf2cd1ddae2513f8d851d8375bd806ea2d11ca
Patrick Woodcock <patrick@ralphmedia.co.uk>
2019-11-14 14:27:45 +0000

    also update seo title

--- yoast-bulk-meta.php
+++ yoast-bulk-meta.php
@@ -4,7 +4,7 @@ Plugin Name: Yoast bulk meta
 Description: 
 Author: Patrick Woodcock
 Author URI: 
-Version: 1.0.0
+Version: 1.0.1
 Requires at least: 5.0
 Tested up to: 5.2
 Requires PHP: 7.0
@@ -24,6 +24,23 @@ function yoast_bulk_meta_register_settings()
 }
 add_action('admin_init', 'yoast_bulk_meta_register_settings');
 
+// _yoast_wpseo_title 
+// '_yoast_wpseo_metadesc'
+function yoast_bulk_meta_update_meta($fieldtitle,$field,$newvalue,$i,$post_id,$doupdate,$url,&$results) {
+	$current = get_post_meta($post_id, $field, true);
+	if ($current == $newvalue) {
+		$results[] = ("<span style='color:blue'>Line " . ($i + 1) . ": post id " . $post_id . " for url " . $url . " " . $fieldtitle . " is the same: " . $newvalue . "</span>");
+	} else if ($current) {
+		$results[] = ("<span style='color:darkorange'>Line " . ($i + 1) . ": post id " . $post_id . " for url " . $url . " current " . $fieldtitle . ": <del>" . $current . "</del> to be replaced with: " . $newvalue . '</span>');
+	} else {
+		$results[] = ("<span style='color:green'>Line " . ($i + 1) . ": post id " . $post_id . " for url " . $url . " add " . $fieldtitle . ": " . $newvalue . "</span>");
+	}
+	if ($doupdate && $current != $newvalue) {
+		update_post_meta($post_id, $field, $newvalue);
+		$results[] = $fieldtitle . " updated.";
+	}
+}
+
 function yoast_bulk_meta_options_page()
 {
 	//content on page goes here
@@ -36,27 +53,23 @@ function yoast_bulk_meta_options_page()
 
 			$csv = str_getcsv($line, "\t");
 
-			if (count($csv) != 2) {
-				$results[] = "Line " . $i + 1 . " error: needs 2 columns only!";
+			if (count($csv) != 3) {
+				$results[] = "Line " . $i + 1 . " error: needs 3 columns exactly!";
 			} else {
 				$url = $csv[0];
 				
 				$url = preg_replace('/^https?\:\/\/[^\/]+/', '', $url);
-				$description = stripslashes($csv[1]);
+				$title = stripslashes($csv[1]);
+				$description = stripslashes($csv[2]);
 				$post_id = url_to_postid($url);
 				if (!$post_id) {
 					$results[] = ("<span style='color:red'>Line " . ($i + 1) . " error: cannot find post for url " . $url . "</span>");
 				} else {
-					$current_metadesc = get_post_meta($post_id, '_yoast_wpseo_metadesc', true);
-					if ($current_metadesc == $description) {
-						$results[] = ("<span style='color:blue'>Line " . ($i + 1) . ": post id " . $post_id . " for url " . $url . " meta is the same: " . $description . "</span>");
-					} else if ($current_metadesc) {
-						$results[] = ("<span style='color:darkorange'>Line " . ($i + 1) . ": post id " . $post_id . " for url " . $url . " current meta: <del>" . $current_metadesc . "</del> to be replaced with: " . $description . '</span>');
-					} else {
-						$results[] = ("<span style='color:green'>Line " . ($i + 1) . ": post id " . $post_id . " for url " . $url . " add meta: " . $description . "</span>");
+					if ($title) {
+						yoast_bulk_meta_update_meta('Title','_yoast_wpseo_title',$title,$i,$post_id,$doupdate,$url,$results);
 					}
-					if ($doupdate) {
-						update_post_meta($post_id, '_yoast_wpseo_metadesc', $description);
+					if ($description) {
+						yoast_bulk_meta_update_meta('Description','_yoast_wpseo_metadesc',$description,$i,$post_id,$doupdate,$url,$results);
 					}
 				}
 			}
@@ -70,8 +83,8 @@ function yoast_bulk_meta_options_page()
 
 		<form method="post" action="">
 			<?php settings_fields('yoast_bulk_meta_options_group'); ?>
-			<h3>Meta descripion CSV</h3>
-			<p>Format: URL [TAB] Meta</p>
+			<h3>Meta CSV</h3>
+			<p>Format: URL [TAB] Title [TAB] Description</p>
 			<textarea style="width:100%; height:500px" name="csvtext"><?= isset($_POST['csvtext']) ? stripslashes($_POST['csvtext']) : ""  ?></textarea>
 			<input type="submit" name="view_only" id="submit" class="button button-primary" value="Check">
 			<ul>
